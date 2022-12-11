09c4c87325d7c1906e0bfb1cbd83f617d3b0149d
cohouseol <cohouseol@gmail.com>
2022-12-11 14:16:48 +0900

    test6

--- src/main/java/com/boardsite/adm/boardsiteadm/BoardSiteAdmApplication.java
+++ src/main/java/com/boardsite/adm/boardsiteadm/BoardSiteAdmApplication.java
@@ -2,8 +2,10 @@ package com.boardsite.adm.boardsiteadm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BoardSiteAdmApplication {
 
     public static void main(String[] args) {
