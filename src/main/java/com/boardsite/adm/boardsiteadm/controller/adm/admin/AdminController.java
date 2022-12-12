bae6c2b70259fa886f575186a9cb9f3ca54b0a56
cohouseol <cohouseol@gmail.com>
2022-12-12 11:33:10 +0900

    swagger 추가

--- src/main/java/com/boardsite/adm/boardsiteadm/controller/adm/admin/AdminController.java
+++ src/main/java/com/boardsite/adm/boardsiteadm/controller/adm/admin/AdminController.java
@@ -1,12 +1,13 @@
 package com.boardsite.adm.boardsiteadm.controller.adm.admin;
 
-
 import com.boardsite.adm.boardsiteadm.dto.request.user.TripUserLoginRequest;
 import com.boardsite.adm.boardsiteadm.dto.response.Response;
 import com.boardsite.adm.boardsiteadm.dto.response.travel.TravelAgencyResponse;
 import com.boardsite.adm.boardsiteadm.dto.response.user.TripUserLoginResponse;
 import com.boardsite.adm.boardsiteadm.dto.security.TripUserPrincipal;
 import com.boardsite.adm.boardsiteadm.service.adm.admin.AdminService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Api(tags ={"관리자 로그인 정보 제공하는 Controller"})
 @RestController
 @RequestMapping("/api/adm/admin")
 @RequiredArgsConstructor
@@ -24,6 +26,8 @@ public class AdminController {
 
 
     @PostMapping("/login")
+    @ApiOperation(value = "로그인", notes = "관리자 로그인을 한다.")
+
     public Response<TripUserLoginResponse> login(@RequestBody TripUserLoginRequest request) {
         String token = adminService.login(request.email(),request.password());
         return Response.success(new TripUserLoginResponse(token));
